2d6b3ac93425ddf4d1d4ceaf72f31273327fc8d0
mawonde <mawondep@gmail.com>
2023-02-16 10:22:47 -0800

    Added 0-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - The Entry point of this function
+ *
+ * Return: 0 indicates Success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - The Entry point of this function
+ *
+ * Return: 0 indicates Success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
